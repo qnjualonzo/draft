4e86ff039517a1fc012951995ae4bc032b45532f
qnjualonzo <qnjualonzo@tip.edu.ph>
2024-12-12 15:34:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 from transformers import pipeline
-from transformers import BertTokenizer, BertForSequenceClassification
-from transformers import pipeline as summarizer_pipeline
 import re
 
 # Load the translation pipeline (English to French)
 translation_pipeline = pipeline("translation", model="Helsinki-NLP/opus-mt-en-fr")
-
-# Load summarization pipeline
-summarizer = summarizer_pipeline("summarization")
 
 # Function to add spaces between sentences
 def add_spaces_between_sentences(text):
@@ -18,8 +13,6 @@ def add_spaces_between_sentences(text):
 # Initialize session state variables
 if "translated_text" not in st.session_state:
     st.session_state.translated_text = ""
-if "summarized_text" not in st.session_state:
-    st.session_state.summarized_text = ""
 
 if "lang_direction" not in st.session_state:
     st.session_state.lang_direction = "EN to FR"
@@ -27,7 +20,7 @@ if "input_text" not in st.session_state:
     st.session_state.input_text = ""
 
 # Streamlit Title
-st.title("Translation and Summarization")
+st.title("Translation")
 
 # Language Direction
 lang_direction = st.sidebar.radio("Select Translation Direction", ["EN to FR", "FR to EN"])
@@ -37,7 +30,6 @@ if lang_direction != st.session_state.lang_direction:
     st.session_state.lang_direction = lang_direction
     st.session_state.input_text = ""
     st.session_state.translated_text = ""
-    st.session_state.summarized_text = ""
 
 # Input Text Area for entering text
 st.session_state.input_text = st.text_area("Enter text to translate:", value=st.session_state.input_text)
@@ -53,21 +45,7 @@ if st.button("Translate"):
         translated_result = translation_pipeline(st.session_state.input_text)
         st.session_state.translated_text = translated_result[0]['translation_text']
         st.session_state.translated_text = add_spaces_between_sentences(st.session_state.translated_text)
-        st.session_state.summarized_text = ""
 
 # Show Translated Text
 if st.session_state.translated_text:
     st.text_area("Translated Text:", value=st.session_state.translated_text, height=150, disabled=True)
-
-    # Summarize the translated text
-    if st.button("Summarize"):
-        if st.session_state.translated_text.strip():
-            processed_text = add_spaces_between_sentences(st.session_state.translated_text)
-
-            # Perform summarization
-            summarized_result = summarizer(processed_text, max_length=150, min_length=50, do_sample=False)
-            st.session_state.summarized_text = summarized_result[0]['summary_text']
-
-# Show Summarized Text
-if st.session_state.summarized_text:
-    st.text_area("Summarized Text:", value=st.session_state.summarized_text, height=150, disabled=True)
